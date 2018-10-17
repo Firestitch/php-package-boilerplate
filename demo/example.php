72684e9c48cf91491446f2091cc894e15a86ab18
Raymond Gigliotti <ray@firestitch.com>
2018-10-17 12:21:43 -0400

    initial commit

---
+++ demo/example.php
@@ -0,0 +1,15 @@
+adasdasd
+
+
+
+
+	// $pdf = new PdfRenderer();
+	// $pdf->SetPrintHeader(false);
+	// $pdf->SetPrintFooter(false);
+	// $pdf->AddPage();
+	// $pdf->setSourceFile("crop-marks.pdf");
+
+	// $tplId = $pdf->importPage(1);
+
+	// $pdf->useTemplate($tplId, 0, 0, 100);
+	// $pdf->Output();
